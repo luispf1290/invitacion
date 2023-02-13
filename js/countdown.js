e0b1c9623fd733af02a6190b8cf31e18c0326952
Luis <luispia1290@gmail.com>
2023-02-13 17:18:35 -0600

    se cambio configuracion

--- js/countdown.js
+++ js/countdown.js
@@ -2,8 +2,8 @@ simplyCountdown('#cuenta', {
     year: 2023, // required
     month: 5, // required
     day: 10, // required
-    hours: 24, // Default is 0 [0-23] integer
-    minutes: 0, // Default is 0 [0-59] integer
+    hours: 7, // Default is 0 [0-23] integer
+    minutes: 06, // Default is 0 [0-59] integer
     seconds: 0, // Default is 0 [0-59] integer
     words: { //words displayed into the countdown
         days: { singular: 'Dia', plural: 'Dias' },
