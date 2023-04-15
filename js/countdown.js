d180d59a3af4d957279fd627e66005a1fb947eda
Luis <luispia1290@gmail.com>
2023-04-15 09:42:10 -0600

    Se agrego funcion para quitar btn de asistencia

--- js/countdown.js
+++ js/countdown.js
@@ -3,8 +3,8 @@ simplyCountdown('#cuenta', {
     month: 4, // required
     day: 17, // required
     hours: 23, // Default is 0 [0-23] integer
-    minutes: 06, // Default is 0 [0-59] integer
-    seconds: 0, // Default is 0 [0-59] integer
+    minutes: 59, // Default is 0 [0-59] integer
+    seconds: 59, // Default is 0 [0-59] integer
     words: { //words displayed into the countdown
         days: { singular: 'Dia', plural: 'Dias' },
         hours: { singular: 'Hora', plural: 'Horas' },
@@ -16,7 +16,10 @@ simplyCountdown('#cuenta', {
     inlineClass: 'simply-countdown-inline', //inline css span class in case of inline = true
     // in case of inline set to false
     enableUtc: true, //Use UTC or not - default : false
-    onEnd: function() { return; }, //Callback on countdown end, put your own function here
+    onEnd: function() { 
+        document.getElementById('asistencia').style.display = 'none'
+        return; 
+    }, //Callback on countdown end, put your own function here
     refresh: 1000, // default refresh every 1s
     sectionClass: 'simply-section', //section css class
     amountClass: 'simply-amount', // amount css class
